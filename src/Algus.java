7e424b39ee7ddd27fb5234d993b9714db131074a
Kerli-Helen Oissar <kerlihelen@hotmail.com>
2016-10-27 20:13:45 +0300

    Tegin JavaFX suure pea akna

--- src/Algus.java
+++ src/Algus.java
@@ -1,11 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * Created by kho on 26.09.2016.
  */
-public class Algus {
+public class Algus extends Application {
     public static void main (String[] args) {
-
-        System.out.println("Ahoi");
-        System.out.println("Mina olen Kerli");
-
+       launch(args);
+    }
+    public void start(Stage suurAken) {
+        suurAken.setTitle("Seebimull");
+        suurAken.show();
     }
 }
